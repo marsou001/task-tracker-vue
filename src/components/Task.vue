2ff753af5584af9cd70932e0ebca4cfde3000799
Marouane Souda <marsou431@gmail.com>
2021-11-02 13:09:56 +0100

    feat: delete tasks

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -2,7 +2,7 @@
   <div :class="[task.reminder ? 'reminder': '', 'task']">
     <h3>
         {{ task.text }}
-        <i class="fas fa-times"></i>
+        <i class="fas fa-times" @click="onDelete(task.id)"></i>
     </h3>
     <p>{{ task.day }}</p>
   </div>
@@ -14,6 +14,11 @@ export default {
   props: {
     task: Object,
   },
+  methods: {
+      onDelete(id) {
+          this.$emit("delete-task", id);
+      }
+  }
 };
 </script>
 
--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,6 +1,6 @@
 <template>
     <div :key="task.id" v-for="task in tasks">
-        <Task :task="task" />
+        <Task :task="task" @delete-task="$emit('delete-task', task.id)" />
     </div>
 </template>
 
@@ -13,6 +13,7 @@
         },
         components: {
             Task,
-        }
+        },
+        emits: ['delete-task'],
     }
 </script>
